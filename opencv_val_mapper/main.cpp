952f603f5c4f304badf1ae7cafc63c857f3f1971
Christian Dipert <cxd1709@mavs.uta.edu>
2022-05-31 12:37:39 -0500

    correctly loads webcam

--- opencv_val_mapper/main.cpp
+++ opencv_val_mapper/main.cpp
@@ -4,6 +4,28 @@
 using namespace std;
 using namespace cv;
 
-int main() {
-	cout << "test" << endl;
+const int fps = 30; // declare constant FPS variable
+
+int main(int argv, char** argc) 
+{
+	
+	Mat frame; // initialize "frame" of type matrix
+
+	VideoCapture vid(0); // create videocapture object
+	if (!vid.isOpened()) {
+		return -1; // if video isnt opened, we shouldn't be doing anything
+	}
+
+	while (vid.read(frame)) { // while constantly reading each frame at speed "fps"
+		
+		imshow("Webcam", frame);
+		if (waitKey(1000 / fps) >= 0) { // wait for any pressed key
+			break;
+		}
+	}
+	return 1;
+
+
+
 }
+
